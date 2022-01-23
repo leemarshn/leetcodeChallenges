d7cd342cafe2cd2d6e6d5bd131ba119486567694
Lee Ndegwa <leendegwa@gmail.com>
2022-01-23 10:44:35 +0300

    2022

---
+++ src/com/company/TwoSum.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * @author - LeeN
+ * PROJECT NAME: LeetcodeChallenges
+ * CREATED ON: Sunday 23 January 2022 - 10:40 AM
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2,3,4,5,6};
+        int target = 9;
+
+        for (int i=0; i<nums.length; i++){
+
+            System.out.println(nums[i]);
+        }
+
+    }
+}
